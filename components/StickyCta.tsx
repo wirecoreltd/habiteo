c1ed94bb32805a90ca43fa10cea3353d947cf963
wirecoreltd <wirecoreltd@gmail.com>
2026-07-13 11:09:52 +0400

    Create StickyCta.tsx

---
+++ components/StickyCta.tsx
@@ -0,0 +1,9 @@
+export default function StickyCta() {
+  return (
+    <div className="sticky-cta">
+      <a href="#lead-form" className="btn btn-primary">
+        Être rappelé gratuitement
+      </a>
+    </div>
+  );
+}
